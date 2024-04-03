7d45bbda65b89596f22090d8bc1014945265a1cd
melkoff <vasamelko@gmail.com>
2024-04-03 18:49:37 +0300

    some fix with img colors

--- src/js/script.js
+++ src/js/script.js
@@ -3,14 +3,16 @@ const menuLinks = document.querySelectorAll('.main-menu li');
 
 // add events
 menuLinks.forEach(link => {
-
    link.addEventListener('click', function (event) {
       event.preventDefault();
 
-      // deleate class from other links
+      // other links will be by default
       menuLinks.forEach(link => {
          link.classList.remove('active-link');
          link.querySelector('a').style.color = '';
+         link.querySelector('svg path').style.stroke = '';
+         link.querySelector('.icon').style.display = '';
+         link.querySelector('.icon-white').style.display = '';
       });
 
       // add class to element <li>
@@ -19,6 +21,18 @@ menuLinks.forEach(link => {
       // change link color
       const linkColor = this.querySelector('a');
       linkColor.style.color = '#fff';
+
+      // change icon
+      const iconMain = this.querySelector('.icon');
+      iconMain.style.display = 'none';
+
+      // change icon white
+      const iconWhite = this.querySelector('.icon-white');
+      iconWhite.style.display = 'block';
+
+      // change SVG color
+      const svgIconColor = this.querySelector('svg path');
+      svgIconColor.style.stroke = '#fff';
    });
 
 });
